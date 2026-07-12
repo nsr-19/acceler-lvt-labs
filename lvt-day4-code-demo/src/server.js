09fc11989e9350e52b7939be54db4302a9bee6c5
nsr-19 <navjtwt22@gmail.com>
2026-07-12 16:07:01 +0530

    delete day 4 labs

--- lvt-day4-code-demo/src/server.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from './app.js';
-
-const PORT = process.env.PORT || 3000;
-const app = createApp();
-
-app.listen(PORT, () => {
-  console.log(`TaskFlow listening on http://localhost:${PORT}`);
-});
